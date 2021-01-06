de95417f77fdcc1943ff7554ce2ff9e648116cf9
nokh9503 <nokh9503@hanmail.net>
2021-01-06 20:46:16 +0900

    add new Strig Problem

---
+++ String/[프로그래머스]문자열을 정수로 바꾸기.py
@@ -0,0 +1,3 @@
+def solution(s):
+    # int()을 이용해서 문자열을 정수로 바꿈
+    return(int(s))
--- String/[프로그래머스]수박수박수박수박수박수.py
+++ String/[프로그래머스]수박수박수박수박수박수.py
@@ -3,13 +3,11 @@ def solution(n):
     answer = ''
     
     for i in range(n):
-        # n이 짝수이면
+        
         if i % 2 == 0:
             answer += "수"
-        # n이 홀수이면
+        
         else:
             answer += "박"
 
-    print(answer)
-
-solution(8)
+    return(answer)
