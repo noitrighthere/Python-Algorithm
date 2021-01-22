fe3964863022ce14d4edc41e591d4e8e914534c9
nokh9503 <nokh9503@hanmail.net>
2021-01-22 20:48:03 +0900

    Programmers problem

---
+++ [프로그래머스]크레인 인형뽑기.py
@@ -0,0 +1,8 @@
+def solution(board, moves):
+    answer = 0      # 결과값
+    stack = []      # 스택 생성
+
+    
+    print(board[moves[i-1], 
+
+print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])
