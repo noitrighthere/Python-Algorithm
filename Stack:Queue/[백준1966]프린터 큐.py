c8c00dfbdb2e03e03a087afe23f5213e68fe58ef
nokh9503 <nokh9503@hanmail.net>
2021-03-22 23:05:00 +0900

    add new Queue problem

--- Stack:Queue/[백준1966]프린터 큐.py
+++ Stack:Queue/[백준1966]프린터 큐.py
@@ -1,29 +1,23 @@
-from collections import deque
-
 T = int(input())    # 테스트케이스 입력
 
 for _ in range(T):
-    
     N, M = map(int, input().split())    # N: 문서의 개수, M: 몇 번째로 인쇄되었는지 궁금한 문서
-    queue = deque(map(int, input().split()))
-    result = 0
+    queue = list(map(int, input().split()))
+    queue = [(i, idx) for idx, i in enumerate(queue)]
 
-    while queue:
-        temp_max = max(queue)
-        M -= 1
-        temp = queue.popleft()
+    cnt = 0
 
-        if temp_max != temp:
-            queue.append(temp)
+    while True:
+        # 현재 중요도가 가장 높은 문서가 맨 앞에 있으면
+        if queue[0][0] == max(queue, key=lambda x:x[0])[0]:
+            cnt += 1
 
-            if M < 0:
-                M = len(queue)-1
-
-        else:
-            result += 1
-
-            if M == 1:
-                print(result)
+            # 몇 번째로 인쇄되었는지 궁금한 문서일 때
+            if queue[0][1] == M:
+                print(cnt)
                 break
-
-    
+            else:
+                queue.pop(0)
+        # 아니면 앞에 있는 문서를 빼서 뒤로 넣어줌
+        else:
+            queue.append(queue.pop(0))
