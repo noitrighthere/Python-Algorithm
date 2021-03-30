f0a7ad2fa279324b1072fb07832bafec6bed830e
nokh9503 <nokh9503@hanmail.net>
2021-03-30 23:35:22 +0900

    solve Implement problem [백준1074] Z

--- Implement/[백준1074]Z.py
+++ Implement/[백준1074]Z.py
@@ -1,40 +1,33 @@
-def func(n, x, y):
-    global answer
-
-    # 크기가 2 x 2인 한수에 대해서 종결조건을 설정
-    if n == 2:
-        # 왼쪽 위
-        if x == r and y == c:
-            print(answer)
-            return
-        answer += 1
-
-        # 오른쪽 위
-        if x == r and y + 1 == c:
-            print(answer)
-            return
-        answer += 1
-
-        # 왼쪽 아래
-        if x + 1 == r and y == c:
-            print(answer)
-            return
-        answer += 1
-
-        # 오른쪽 아래
-        if x + 1 == r and y + 1 == c:
-            print(answer)
-            return
-        answer += 1
-
-    else:
-        # 지그재그 형태로 4등분하여 각각 재귀함수를 호출함
-        func(n // 2, x, y)
-        func(n // 2, x, y + n // 2)
-        func(n // 2, x + n // 2, y)
-        func(n // 2, x + n // 2, y + n // 2)
-
 N, r, c = map(int, input().split())     # N, r, c 입력
 answer = 0
 
-func(2 ** N, 0, 0)
+while N > 0:
+    temp = (2 ** N) // 2
+    # 큰 사각형에서 점점 작은 사각형으로 이동
+    if N > 1:
+        # 2사분면
+        if temp > r and temp <= c:
+            answer += temp ** 2 # 1사분면 만큼 더해줌
+            c -= temp
+        # 3사분면
+        elif temp <= r and temp > c:
+            answer += (temp**2) * 2 # 2사분면 만큼 더해줌
+            r -= temp
+        # 1사분면
+        elif temp <= r and temp <= c:
+            answer += (temp**2) * 3 # 3사분면 만큼 더해줌
+            r -= temp
+            c -= temp
+
+    # 2 x 2 일때 2~4 사분면을 이동한 횟수를 구함
+    elif N == 1:
+        if r == 0 and c == 1:
+            answer += 1
+        elif r == 1 and c == 0:
+            answer += 2
+        elif r == 1 and c == 1:
+            answer += 3
+    
+    N -= 1
+
+print(answer)
